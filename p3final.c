612841d6e20e8b1a67d8c7cc9864eaac94bbabc0
Revanth Krishna <94678115+SRevanth2003@users.noreply.github.com>
2021-12-20 05:56:55 +0000

    done

--- p3final.c
+++ p3final.c
@@ -1,6 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
-void input(int*a , int*b)
+void input(int *a , int *b)
 {
-  printf("Enter the First Number : \n")
+  printf("Enter the First Number : \n");
+scanf("%d",a);
+printf("enter the Second Number : \n");
+scanf("%d", b);
+}
+void add(int a, int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a, int b,int sum)
+{
+  printf("sum of %d and %d is %d", a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
 }
